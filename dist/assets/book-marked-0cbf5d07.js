1686497b337fb8be85b70c707597818a47221224
Sreekanth <spayyavula@gmail.com>
2025-06-25 07:45:18 -0700

    Deploy application to Netlify

---
+++ dist/assets/book-marked-0cbf5d07.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-b5290ce3.js";const t=o("BookMarked",[["path",{d:"M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20",key:"t4utmx"}],["polyline",{points:"10 2 10 10 13 7 16 10 16 2",key:"13o6vz"}]]);export{t as B};
